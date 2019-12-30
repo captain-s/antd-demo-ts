29b70f6aa8c7f647dcced42e86e7086edaaf8b75
sys-captain <596421009@qq.com>
2019-12-30 10:46:44 +0800

    sys
    第一版

--- src/store/action.ts
+++
@@ -1,8 +0,0 @@
-const SAVETOKEN = 'SAVETOKEN';
-// 保存图片地址
-export const saveToken = (token:string) => {
-    return {
-        type: SAVETOKEN,
-        token,
-    }
-}
--- src/store/reducer.ts
+++
@@ -1,8 +0,0 @@
-export function setToken(state = {}, action:any) {
-    switch (action.type) {
-    case 'SAVETOKEN':
-        return action.token
-    default:
-        return state
-    }
-}
